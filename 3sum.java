fee3ce0b966d4401e20a27cefea4074b77e42c24
Sarang Ajay Patel <sarangp203@gmail.com>
2025-01-02 12:00:00 -0500

    Add 3sum.java (day 2 since 2025-01-01)

---
+++ 3sum.java
@@ -0,0 +1,27 @@
+# 3Sum
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+
+        }
+
+        return new ArrayList<>(set);
+    }
+}
