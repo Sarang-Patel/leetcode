43ecaf82b329c83b9a7a59b27063203beb64070b
Sarang Ajay Patel <sarangp203@gmail.com>
2025-03-01 12:00:00 -0500

    Add single-number.java (backdated commit)

---
+++ single-number.java
@@ -0,0 +1,11 @@
+# Single Number
+class Solution {
+    public int singleNumber(int[] nums) {
+        int s = 0;
+        for(int i : nums) {
+            s = s ^ i;
+        }
+
+        return s;
+    }
+}
