b9388f04dd785329fd3aaa8253075bb06365d00c
Sarang Ajay Patel <sarangp203@gmail.com>
2025-05-01 12:00:00 -0400

    Add jump-game-ii.java (backdated commit)

---
+++ jump-game-ii.java
@@ -0,0 +1,18 @@
+# Jump Game II
+class Solution {
+    public int jump(int[] nums) {
+        int currEnd = 0;
+        int farthest = 0;
+
+        int jumps = 0;
+        for(int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if(i == currEnd) {
+                jumps++;
+                currEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
