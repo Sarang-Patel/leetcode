7f4b744cd96950fb7a31ed12fbe8f3350e89bbcd
Sarang Ajay Patel <sarangp203@gmail.com>
2025-05-02 12:00:00 -0400

    Add jump-game.java (backdated commit)

---
+++ jump-game.java
@@ -0,0 +1,14 @@
+# Jump Game
+class Solution {
+    public boolean canJump(int[] nums) {
+        int lastGoodIndex = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= lastGoodIndex) {
+                lastGoodIndex = i;
+            }
+        }
+
+        return lastGoodIndex == 0;
+    }
+}
