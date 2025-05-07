518a5f4436170fe79d1e1edf3d3d3571d8ca83ed
Sarang Ajay Patel <sarangp203@gmail.com>
2025-05-07 12:00:00 -0400

    Add distinct-subsequences.java (backdated commit)

---
+++ distinct-subsequences.java
@@ -0,0 +1,19 @@
+# Distinct Subsequences
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        long[] dp = new long[m+1];
+        dp[0] = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = m; j >= 1; j--) {
+                if(s.charAt(i) == t.charAt(j-1)) {
+                    dp[j] += dp[j-1];
+                }
+            }
+        }
+
+        return (int) dp[m];
+    }
+}
