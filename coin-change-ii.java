220052502de6bffe2afe59b07ff34fab12f1e014
Sarang Ajay Patel <sarangp203@gmail.com>
2025-05-12 12:00:00 -0400

    Add coin-change-ii.java (backdated commit)

---
+++ coin-change-ii.java
@@ -0,0 +1,16 @@
+# Coin Change II
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+
+        dp[0] = 1;
+
+        for(int coin : coins) {
+            for(int a = coin; a <= amount; a++) {
+                dp[a] += dp[a - coin];
+            }
+        }
+
+        return dp[amount];
+    }
+}
