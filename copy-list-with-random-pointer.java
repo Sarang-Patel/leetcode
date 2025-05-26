721e1243b382a90c93334d33ef90a9010e17d2ca
Sarang Ajay Patel <sarangp203@gmail.com>
2025-05-26 12:00:00 -0400

    Add copy-list-with-random-pointer.java (backdated commit)

---
+++ copy-list-with-random-pointer.java
@@ -0,0 +1,51 @@
+# Copy List with Random Pointer
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        Node curr = head;
+
+        while(curr != null) {
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
+        }
+
+        curr = head;
+        while(curr != null) {
+            if(curr.random != null)
+            curr.next.random = curr.random.next;
+
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        Node copyHead = head.next;
+
+        while(curr != null) {
+            Node node = curr.next;
+            curr.next = curr.next.next;
+            if(node.next != null) {
+                node.next = node.next.next;
+            }
+            curr = curr.next;
+        }
+
+        return copyHead;
+    }
+}
